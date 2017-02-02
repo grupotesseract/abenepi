63488b2c55b9beb500bdf586710c4972c2dbaa87
vagrant <vagrant@homestead>
2017-02-02 23:23:39 +0000

    Inicio Dev

---
+++ database/migrations/2017_02_02_224905_create_inscritos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInscritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //TABELA JÁ EXISTIA POR ISSO O DROP 
+        Schema::drop('inscritos');
+        
+        Schema::create('inscritos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('endereco_casa');
+            $table->string('profissao');
+            $table->string('local_de_trabalho');
+            $table->string('endereco_trabalho');              
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscritos');
+    }
+}
